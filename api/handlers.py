7303bc28b4dd86a10e8ff94ec1efdc550767b2e0
Ali Raza Zaidi <alirzaidi02@gmail.com>
2024-03-27 21:48:44 -0400

    Return list of player name + id dicts

--- api/handlers.py
+++ api/handlers.py
@@ -8,6 +8,6 @@ def predict_handler(event, context):
 
 def players_handler(event, context):
     try:
-        return {"statusCode": 200, "body": players_dict()}
+        return {"statusCode": 200, "body": [{"name": key, "playerId": value} for key, value in players_dict().items()]}
     except Exception as e:
         return {"error": str(e)}
